6162ec98c4b7cb301d381884136fba191a0ca79d
Naoto Shioya <shioya.naoto17@gmail.com>
2021-10-31 12:24:25 +0900

    ✨ responsive test

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -4,16 +4,31 @@ import Image from "next/image";
 export const Top = () => {
   return (
     <Box mt="100">
-      <Flex align="center" justify="space-between">
-        <Box ml="20">
-          <Box ml="20" mb="10">
-            <Image src="/images/top.png" width={343.61} height={306} />
+      <Box d={["none", "block"]}>
+        <Flex align="center" justify="space-between">
+          <Box ml="20">
+            <Box ml="20" mb="10">
+              <Image src="/images/top.png" width={343.61} height={306} />
+            </Box>
+            <Image src="/images/store.png" width={460.42} height={70.03} />
           </Box>
-          <Image src="/images/store.png" width={460.42} height={70.03} />
-        </Box>
 
-        <Image src="/images/preview.png" width={672} height={596.39} />
-      </Flex>
+          <Box>
+            <Image src="/images/preview.png" width={672} height={596.39} />
+          </Box>
+        </Flex>
+      </Box>
+
+      <Box mt="300" d={["block", "none"]}>
+        <Center>
+          <Box mb="20" ml="20">
+            <Image src="/images/top.png" width={635.69} height={541} />
+          </Box>
+        </Center>
+        <Center>
+          <Image src="/images/store.png" width={734} height={111.65} />
+        </Center>
+      </Box>
     </Box>
   );
 };
--- src/components/What.tsx
+++ src/components/What.tsx
@@ -3,36 +3,70 @@ import Image from "next/image";
 
 export const What = () => {
   return (
-    <Box mt="100">
-      <Center>
-        <Heading
-          as="h1"
-          size="2xl"
-          fontFamily="Days One, sans-serif"
-          fontWeight="100"
-        >
-          MAGNET
-        </Heading>
-        <Heading as="h1" size="2xl">
-          とは
-        </Heading>
-      </Center>
+    <>
+      <Box mt="100" d={["none", "block"]}>
+        <Center>
+          <Heading
+            as="h1"
+            size="2xl"
+            fontFamily="Days One, sans-serif"
+            fontWeight="100"
+          >
+            MAGNET
+          </Heading>
+          <Heading as="h1" size="2xl">
+            とは
+          </Heading>
+        </Center>
 
-      <Center mt="100">
-        <Flex align="center">
-          <Box mr="100">
-            <Heading as="h2" size="xl">
-              友達の暇が一目でわかる掲示板SNS
-            </Heading>
-            <Box w="500px" mt="5">
-              <p>
-                暇という無限大のエネルギーを「友達や大切な人と遊ぶ」というかけがえのない幸せへと変換します。
-              </p>
+        <Center mt="100">
+          <Flex align="center">
+            <Box mr="100">
+              <Heading as="h2" size="xl">
+                友達の暇が一目でわかる掲示板SNS
+              </Heading>
+              <Box w="500px" mt="5">
+                <p>
+                  暇という無限大のエネルギーを「友達や大切な人と遊ぶ」というかけがえのない幸せへと変換します。
+                </p>
+              </Box>
             </Box>
-          </Box>
-          <Image src="/images/3display.png" width={458} height={481} />
-        </Flex>
-      </Center>
-    </Box>
+            <Image src="/images/3display.png" width={458} height={481} />
+          </Flex>
+        </Center>
+      </Box>
+
+      <Box mt="300" d={["block", "none"]}>
+        <Center>
+          <Heading
+            as="h1"
+            size="4xl"
+            fontFamily="Days One, sans-serif"
+            fontWeight="100"
+          >
+            MAGNET
+          </Heading>
+          <Heading as="h1" size="4xl">
+            とは
+          </Heading>
+        </Center>
+        <Center>
+          <Heading as="h1" size="3xl" mt="10">
+            友達の暇が一見でわかる掲示板SNS
+          </Heading>
+        </Center>
+
+        <Center mt="40">
+          <Image src="/images/3display.png" width={658} height={691.35} />
+        </Center>
+
+        <Center>
+          <Heading as="h1" size="2xl" mt="40" w="700px">
+            暇という無限大のエネルギーを 「友達や大切な人と遊ぶ」という
+            かけがえのない幸せへと変換します。
+          </Heading>
+        </Center>
+      </Box>
+    </>
   );
 };
