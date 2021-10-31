90344cc8e0900fb678863fdcc51b571ee2afb76b
Naoto Shioya <shioya.naoto17@gmail.com>
2021-10-31 16:26:30 +0900

    ✨ responsive Howtouse

--- src/components/Howtouse.tsx
+++ src/components/Howtouse.tsx
@@ -87,6 +87,58 @@ export const Howtouse = () => {
             の使い方
           </Heading>
         </Center>
+
+        <Box mt="50">
+          <Center mt="30">
+            <Image src="/images/step1.png" width={145} height={145} />
+            <Heading as="h2" size="xl" mx="30">
+              仲の良い友達やサークルのメンバーとコネクト！
+            </Heading>
+            <Divider orientation="horizontal" w="200px"></Divider>
+          </Center>
+
+          <Center mt="10" mr="300">
+            <Image src="/images/friends.png" width={280} height={333.31} />
+            <Heading as="h2" size="xl" ml="100">
+              仲の良い友達と！！
+            </Heading>
+          </Center>
+
+          <Center ml="300">
+            <Heading as="h2" size="xl" mr="100">
+              サークルや部活のメンバーと！
+            </Heading>
+            <Image src="/images/member.png" width={334} height={235.97} />
+          </Center>
+        </Box>
+
+        <Box mt="50">
+          <Center mt="30">
+            <Image src="/images/step2.png" width={145} height={145} />
+            <Heading as="h2" size="xl" mx="30">
+              タイムラインや作ったグループに暇な日時、遊ぶ内容を投稿！
+            </Heading>
+            <Divider orientation="horizontal" w="100px"></Divider>
+          </Center>
+
+          <Center mt="20">
+            <Image src="/images/step2img.png" width={625} height={481.58} />
+          </Center>
+        </Box>
+
+        <Box mt="50">
+          <Center mt="30">
+            <Image src="/images/step3.png" width={145} height={145} />
+            <Heading as="h2" size="xl" mx="30">
+              暇な友達とマッチング！
+            </Heading>
+            <Divider orientation="horizontal" w="200px"></Divider>
+          </Center>
+
+          <Center mt="20">
+            <Image src="/images/step3img.png" width={263} height={384} />
+          </Center>
+        </Box>
       </Box>
     </>
   );
